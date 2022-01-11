b35477a2d797e38e8c6802e248a9e99c8d8d4692
EC2 Default User <ec2-user@ip-172-31-59-184.ec2.internal>
2022-01-11 09:34:40 +0000

    save all

--- aws/cognito/typescript.ts
+++ aws/cognito/typescript.ts
@@ -1,11 +1,14 @@
 import Swal from 'sweetalert2'
 import {CognitoUser, ISignUpResult} from 'amazon-cognito-identity-js'
 import { Auth } from 'aws-amplify'
+
 // Setup the configuration
 Auth.configure({
     userPoolId:'us-east-1_dqiapzBbr',
     userPoolWebClientId:'57a80fik1473v2up522vav7br2',
 })
+
+var cureentUserNAme: 
 
 function onResendConfirmationCode(){
  Auth.resend();
